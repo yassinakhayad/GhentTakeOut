302e3f9d31a7c747637d9425155299ece2d3a55e
yassin akhayad <yassin.akhayad@oxynade.com>
2013-05-16 19:55:09 +0200

    initiate project

---
+++ GhentApp/assets/www/js/compass.js
@@ -0,0 +1,26 @@
+
+		//
+	    // Start watching the acceleration
+	    //
+	    function startWatchCompass() {
+	        // Update acceleration every 25ms
+	        var options = { frequency: 100 };
+	        watchID = navigator.compass.watchHeading(compassSuccess, onError, options);
+
+	    }
+	
+	    // Stop watching the acceleration
+	    //
+	    function stopWatchAccelerometer() {
+	        if (watchID) {
+	            navigator.compass.clearWatch(watchID);
+	            watchID = null;
+	        }
+	    }
+	
+	    // onSuccess: Get a snapshot of the current acceleration
+	    //
+	    function compassSuccess(heading) {
+	    	alert(heading.magneticHeading);
+	    }
+	
